20750452e3174ed98717c4cb645ebbdf0e5f9909
Shay Skitel <shay.skitel@gmail.com>
2022-04-14 17:38:22 +0300

    Add painting functionality

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const boardSize = 16;
 for (let i = 0; i < boardSize * boardSize; i++) {
     const newSquare = document.createElement("div");
     newSquare.classList.add("square");
-
     board.appendChild(newSquare);
+
+    // Add event listener to all squares on mouse enter
+    newSquare.addEventListener("mouseenter", function () {
+        this.classList.add("painted");
+    });
 }
